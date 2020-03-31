a5b5891e20e784cf9f4f23f149dd7dd478f83e04
yanxin152133 <yanxin152133@gmail.com>
2020-03-31 22:46:07 +0800

    Login-demo

---
+++ recommend-clint/src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <!--prettier-ignore-->
+    <div>
+
+
+        <b-card
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+        >
+
+            <loginForm></loginForm>
+            <b-button href="#" variant="primary">login</b-button>
+        </b-card>
+    </div>
+</template>
+
+<script>
+import loginForm from "../components/loginForm";
+export default {
+    name: "Login",
+    components: { loginForm }
+};
+</script>
+
+<style lang="less"></style>
